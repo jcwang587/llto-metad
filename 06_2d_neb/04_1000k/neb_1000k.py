4857ba33fe98d492e4abec26350e6b1b01e233c8
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-07-03 17:48:14 -0400

    first upload

--- 06_2d_neb/04_1000k/neb_1000k.py
+++ 06_2d_neb/04_1000k/neb_1000k.py
@@ -2,15 +2,12 @@ from xdatbus import neb_2d, local_minima
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 fes = np.load("y_fes_1000k.npy")
-
-# copy fes along the reaction coordinate 1
 mirrored_fes = np.flip(fes, axis=1)
 fes = np.concatenate((mirrored_fes, fes), axis=1)
 
 # remove the middle 13 columns of 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69
-fes = np.delete(fes, [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69], axis=1)
+fes = np.delete(fes, [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70], axis=1)
 
 local_minima_coords = local_minima(fes, size=20)
 n_images = 100
@@ -34,18 +31,53 @@ plt.ylabel("Reaction coordinate 2")
 for i, (x, y) in enumerate(local_minima_coords):
     plt.text(y, x, f"{i}", color="red")
 plt.colorbar()
-plt.savefig("mep_1_1000k.png")
+plt.savefig("mep_1000k_path6-mir-rev.png")
 plt.show()
 
-np.save("mep_fes_1_1000k.npy", mep_fes_1_1000k)
+np.save("mep_fes_1000k_path6-mir-rev.npy", mep_fes_1_1000k)
+
 
 fes = np.load("y_fes_1000k.npy")
+mirrored_fes = np.flip(fes, axis=1)
+fes = np.concatenate((fes, mirrored_fes), axis=1)
 
-# copy fes along the reaction coordinate 2
-fes = np.tile(fes, (2, 1))
+# remove the middle 13 columns of 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69
+fes = np.delete(fes, [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70], axis=1)
 
-# remove the middle 13 rows of 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69
-fes = np.delete(fes, [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69], axis=0)
+local_minima_coords = local_minima(fes, size=20)
+n_images = 100
+n_steps = 1000
+spring_constant = 0.2
+
+mep_1_1000k, mep_fes_1_1000k = neb_2d(
+    fes,
+    local_minima_coords[0],
+    local_minima_coords[1],
+    n_images,
+    n_steps,
+    spring_constant,
+)
+
+# plot the minimum energy path
+plt.imshow(fes, cmap="viridis")
+plt.plot(mep_1_1000k[:, 1], mep_1_1000k[:, 0], "ro-")
+plt.xlabel("Reaction coordinate 1")
+plt.ylabel("Reaction coordinate 2")
+for i, (x, y) in enumerate(local_minima_coords):
+    plt.text(y, x, f"{i}", color="red")
+plt.colorbar()
+plt.savefig("mep_1000k_path6-mir.png")
+plt.show()
+
+np.save("mep_fes_1000k_path6-mir.npy", mep_fes_1_1000k)
+
+
+fes = np.load("y_fes_1000k.npy")
+mirrored_fes = np.flip(fes, axis=0)
+fes = np.concatenate((mirrored_fes, fes), axis=0)
+
+# remove the middle 14 rows of 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69
+fes = np.delete(fes, [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70], axis=0)
 
 local_minima_coords = local_minima(fes, size=20)
 n_images = 100
@@ -69,7 +101,42 @@ plt.ylabel("Reaction coordinate 2")
 for i, (x, y) in enumerate(local_minima_coords):
     plt.text(y, x, f"{i}", color="red")
 plt.colorbar()
-plt.savefig("mep_2_1000k.png")
+plt.savefig("mep_1000k_path5-mir-rev.png")
 plt.show()
 
-np.save("mep_fes_2_1000k.npy", mep_fes_2_1000k)
+np.save("mep_1000k_path5-mir-rev.npy", mep_fes_2_1000k)
+
+
+fes = np.load("y_fes_1000k.npy")
+mirrored_fes = np.flip(fes, axis=0)
+fes = np.concatenate((fes, mirrored_fes), axis=0)
+
+# remove the middle 14 rows of 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69
+fes = np.delete(fes, [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70], axis=0)
+
+local_minima_coords = local_minima(fes, size=20)
+n_images = 100
+n_steps = 1000
+spring_constant = 0.2
+
+mep_2_1000k, mep_fes_2_1000k = neb_2d(
+    fes,
+    local_minima_coords[0],
+    local_minima_coords[1],
+    n_images,
+    n_steps,
+    spring_constant,
+)
+
+# plot the minimum energy path
+plt.imshow(fes, cmap="viridis")
+plt.plot(mep_2_1000k[:, 1], mep_2_1000k[:, 0], "ro-")
+plt.xlabel("Reaction coordinate 1")
+plt.ylabel("Reaction coordinate 2")
+for i, (x, y) in enumerate(local_minima_coords):
+    plt.text(y, x, f"{i}", color="red")
+plt.colorbar()
+plt.savefig("mep_1000k_path5-mir.png")
+plt.show()
+
+np.save("mep_1000k_path5-mir.npy", mep_fes_2_1000k)
