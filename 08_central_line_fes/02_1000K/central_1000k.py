26d679b3ad5322edb94e4b21f773e22e26b132b6
Jiacheng Wang <kashkingwong@gmail.com>
2024-07-03 16:03:28 -0400

    update 3d

--- 08_central_line_fes/02_1000K/central_1000k.py
+++ 08_central_line_fes/02_1000K/central_1000k.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-fes = np.load("y_fes_700k.npy")
+fes = np.load("y_fes_1000k.npy")
 
 # remove the middle 13 columns of 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69
 fes = np.delete(fes, [0, 1, 2, 3, 4, 5, 58, 59, 60, 61, 62, 63], axis=1)
@@ -12,5 +12,5 @@ fes = np.delete(fes, [0, 1, 2, 3, 4, 5, 58, 59, 60, 61, 62, 63], axis=0)
 mid_1_700k = fes[26, :] - np.min(fes[26, :])
 mid_2_700k = fes[:, 26] - np.min(fes[:, 26])
 
-np.save("mid_fes_1_700k.npy", mid_1_700k)
-np.save("mid_fes_2_700k.npy", mid_2_700k)
+np.save("mid_fes_1_1000k.npy", mid_1_700k)
+np.save("mid_fes_2_1000k.npy", mid_2_700k)
