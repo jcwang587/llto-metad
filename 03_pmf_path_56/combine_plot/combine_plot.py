693314cddfd4161c7fcce2fdcc7934854f2002a1
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-07-05 19:42:59 -0400

    first upload

--- 03_pmf_path_56/combine_plot/combine_plot.py
+++ 03_pmf_path_56/combine_plot/combine_plot.py
@@ -24,10 +24,11 @@ fig, ax1 = plt.subplots(figsize=(10, 6))
 ax1.set_xlabel(r'$\xi_2$', fontsize=20, labelpad=10)
 ax1.set_ylabel('Potential of Mean Force (eV)', fontsize=20, labelpad=10)
 
-ax1.plot(a_1000k, fes_a_1000k, color='#1a80bb', lw=5)
-ax1.plot(c_1000k, fes_c_1000k, color='#ea801c', lw=2)
 ax1.plot(a_700k, fes_a_700k, color='#1a80bb', lw=2, ls='--')
 ax1.plot(c_700k, fes_c_700k, color='#ea801c', lw=2, ls='--')
+ax1.plot(c_1000k, fes_c_1000k, color='#ea801c', lw=2)
+ax1.plot(a_1000k, fes_a_1000k, color='#1a80bb', lw=5)
+ax1.plot(a_1000k, fes_a_1000k, color='#1a80bb', lw=9, alpha=0.3)
 ax1.tick_params(axis='y', labelsize=16)
 ax1.tick_params(axis='x', labelsize=16)
 ax1.grid(True, which='major', axis='x', linestyle='--')
@@ -53,7 +54,7 @@ ax1.yaxis.set_major_locator(MultipleLocator(0.05))
 ax1.yaxis.set_minor_locator(MultipleLocator(0.01))
 
 # set legend in two columns
-ax1.legend(['1000K Path-5', '1000K Path-6', '700K Path-5', '700K Path-6'],
+ax1.legend(['700K Path-5', '700K Path-6', '1000K Path-5', '1000K Path-6'],
            fontsize=16, loc='upper center', ncol=2)
 
 plt.tight_layout()
