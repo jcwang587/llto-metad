85b75784178f1538760f41d82cb71a6a44d0ac70
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2024-07-06 12:08:11 -0400

    first upload

--- 09_cylindrical_wall_plot/01_100K/path_based_cv.py
+++ 09_cylindrical_wall_plot/01_100K/path_based_cv.py
@@ -1,6 +1,6 @@
 import numpy as np
+import alphashape
 from matplotlib import pyplot as plt
-import plotly.graph_objects as go
 import seaborn as sns
 from statsmodels.nonparametric.smoothers_lowess import lowess
 
@@ -11,12 +11,18 @@ def calculate_length(mep):
     return np.sum(segment_lengths)
 
 
-# define the constants
-kb = 8.617333262145e-5
-t = 100
+def get_alpha_shape(points, alpha=0.05):
+    if len(points) > 3:
+        alpha_shape = alphashape.alphashape(points[:, [0, 2]], alpha)
+        boundary_points = np.array(list(alpha_shape.exterior.coords))
+    else:
+        boundary_points = points[:, [0, 2]]
+    return boundary_points
+
 
 # load npy file
 fes = np.load('fes_data_3d.npy')
+fes_2d = np.min(fes, axis=1)
 fes_size = fes.shape
 mep_1 = np.load('../../06_2d_neb/01_100k/mep_13_100k.npy')
 mep_2 = np.load('../../06_2d_neb/01_100k/mep_32_100k.npy')
@@ -110,27 +116,54 @@ result_2 = np.column_stack((x_2, y_2, z_2))
 result_3 = np.column_stack((x_3, y_3, z_3))
 result_4 = np.column_stack((x_4, y_4, z_4))
 
-# Create a Plotly figure
-fig = go.Figure()
+# Get alpha shape boundary points for each result
+alpha_shape_1 = get_alpha_shape(result_1, alpha=0.8)
+alpha_shape_2 = get_alpha_shape(result_2, alpha=0.8)
+alpha_shape_3 = get_alpha_shape(result_3, alpha=0.8)
+alpha_shape_4 = get_alpha_shape(result_4, alpha=0.8)
 
-# Adding scatter plots
-fig.add_trace(go.Scatter3d(x=result_1[:, 0], y=result_1[:, 1], z=result_1[:, 2], mode='markers',
-                           marker=dict(size=0.5, color='red'), name='result_1'))
-fig.add_trace(go.Scatter3d(x=result_2[:, 0], y=result_2[:, 1], z=result_2[:, 2], mode='markers',
-                           marker=dict(size=0.5, color='blue'), name='result_2'))
-fig.add_trace(go.Scatter3d(x=result_3[:, 0], y=result_3[:, 1], z=result_3[:, 2], mode='markers',
-                           marker=dict(size=0.5, color='green'), name='result_3'))
-fig.add_trace(go.Scatter3d(x=result_4[:, 0], y=result_4[:, 1], z=result_4[:, 2], mode='markers',
-                           marker=dict(size=0.5, color='yellow'), name='result_4'))
+# Plot the alpha shape boundary points
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111)
 
-# Setting labels
-fig.update_layout(scene=dict(
-    xaxis_title='a',
-    yaxis_title='b',
-    zaxis_title='c'),
-    margin=dict(l=0, r=0, b=0, t=0))
+# increase the font size
+plt.rcParams.update({'font.size': 18})
 
-# Show the plot
-fig.write_html('3d_plot_mep_100k.html')
+plt.imshow(fes_2d, cmap="viridis", extent=(0.0, 64.0, 64.0, 0.0))
+plt.plot(mep_1[:, 1], mep_1[:, 0], "ro-")
+plt.plot(mep_2[:, 1], mep_2[:, 0], "bo-")
+plt.plot(mep_3[:, 1], mep_3[:, 0], "go-")
+plt.plot(mep_4[:, 1], mep_4[:, 0], "yo-")
+plt.xlabel("c", fontsize=28)
+plt.ylabel("a", fontsize=28)
+plt.colorbar()
 
+# Plot the closed alpha shape boundary
+ax.plot(np.append(alpha_shape_1[:, 1], alpha_shape_1[0, 1]),
+        np.append(alpha_shape_1[:, 0], alpha_shape_1[0, 0]), c='r')
+ax.plot(np.append(alpha_shape_2[:, 1], alpha_shape_2[0, 1]),
+        np.append(alpha_shape_2[:, 0], alpha_shape_2[0, 0]), c='b')
+ax.plot(np.append(alpha_shape_3[:, 1], alpha_shape_3[0, 1]),
+        np.append(alpha_shape_3[:, 0], alpha_shape_3[0, 0]), c='g')
+ax.plot(np.append(alpha_shape_4[:, 1], alpha_shape_4[0, 1]),
+        np.append(alpha_shape_4[:, 0], alpha_shape_4[0, 0]), c='y')
 
+# show a 64*64 grid lines
+ax.set_xticks(np.linspace(0, 64, 6))
+ax.set_yticks(np.linspace(0, 64, 6))
+ax.grid(True)
+
+# set the ticks font size
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+
+# Create tick labels, excluding 0
+xtick_labels = [f'{x:.1f}' if x != 0 and x != 1 else '' for x in np.linspace(0, 1, 6)]
+ytick_labels = [f'{y:.1f}' if y != 0 and y != 1 else '' for y in np.linspace(1, 0, 6)]
+
+# Change the tick labels to 0-1 range, excluding 0
+ax.set_xticklabels(xtick_labels)
+ax.set_yticklabels(ytick_labels)
+
+plt.savefig("Figure S1b.tif", dpi=300)
+plt.show()
